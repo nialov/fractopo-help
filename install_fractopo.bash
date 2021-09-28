6d37b314e74972efb88fb31fc6ed2fc7d0b67304
nialov <nikolasovaskainen@hotmail.com>
2021-09-28 11:38:44 +0300

    feat: add csc install script

---
+++ install_fractopo.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/jovyan/work
+
+echo "Cloning fractopo-help repository"
+git clone https://github.com/nialov/fractopo-help.git
+
+echo "Copying network.ipynb notebook to current directory"
+cp fractopo-help/network.ipynb .
+
+echo "Installing fractopo==0.2.1"
+pip install fractopo==0.2.1
