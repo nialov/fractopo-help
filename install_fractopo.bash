5b298e03edd40be262c29e591b349a4a6fdb5c5a
nialov <nikolasovaskainen@hotmail.com>
2021-09-28 12:08:35 +0300

    fix: upgrade pip before install

--- install_fractopo.bash
+++ install_fractopo.bash
@@ -8,5 +8,8 @@ git clone https://github.com/nialov/fractopo-help.git
 echo "Copying network.ipynb notebook to current directory"
 cp fractopo-help/network.ipynb .
 
+echo "Upgrading pip"
+pip install --upgrade pip
+
 echo "Installing fractopo==0.2.1"
 pip install fractopo==0.2.1
